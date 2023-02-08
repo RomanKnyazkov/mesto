39b0f5c1eaa37053b052b0a1506713bacb9c4ca3
RomanKnyazkov <chervat2@yandex.ru>
2023-02-08 23:32:04 +0500

    fix: форма

---
+++ script.js
@@ -0,0 +1,4 @@
+let popupBg = document.querySelector('.popup__bg'); // Фон попап окна
+let popup = document.querySelector('.popup'); // Само окно
+let openPopupButton = document.querySelectorAll('.open-popup'); // Кнопка для показа окна
+let closePopupButton = document.querySelector('.close-popup'); // Кнопка для скрытия окна
