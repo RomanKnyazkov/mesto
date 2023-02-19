5eac7311ceb829e95273fba7e93acc3fe9d7bc6e
RomanKnyazkov <chervat2@yandex.ru>
2023-02-19 13:38:39 +0500

    fix: Ревью
    убрал media 3000;
    исправил JS;
    поправил наименование селекторов;

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,22 @@
-//popup
-
 const buttonOpenPopup = document.querySelector('.profile__pen');
 const popup = document.querySelector('.popup');
 const buttonClosePopup = document.querySelector('.popup__close');
+let popupForm = document.querySelector('.popup__form');
+let popupName = document.querySelector('.popup__input_value_name');
+let popupProfile = document.querySelector('.popup__input_value_profile');
+let title = document.querySelector('.profile__title');
+let profile = document.querySelector('.profile__text');
+
+//popup open.close
 
 const handleOpenButtonClick = () => {
-  popup.classList.add('popup_open');
+  popup.classList.add('popup_opened');
+  popupName.value = title.innerText;
+  popupProfile.value = profile.innerText;
 }
 
 const handleCloseButtonClick = () => {
-  popup.classList.remove('popup_open');
+  popup.classList.remove('popup_opened');
 }
 
 buttonOpenPopup.addEventListener('click', handleOpenButtonClick);
@@ -17,13 +24,7 @@ buttonClosePopup.addEventListener('click', handleCloseButtonClick);
 
 //SaveText
 
-let popupContainer = document.querySelector('.popup__container');
-let popupName = document.querySelector('.popup__input_name');
-let popupProfile = document.querySelector('.popup__input_profile');
-let title = document.querySelector('.profile__title');
-let profile = document.querySelector('.profile__text');
-
-popupContainer.addEventListener('submit', (event) => {
+popupForm.addEventListener('submit', (event) => {
 event.preventDefault();
 
 title.textContent = popupName.value;
